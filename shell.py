6c0e3d3128310097af069f30880a8fdb22c68b58
DoctorStud <drstudonline@gmail.com>
2022-07-02 16:33:41 +0200

    Added basic shell and interpreter

---
+++ shell.py
@@ -0,0 +1,16 @@
+from interpreter import Lexer, Parser
+
+
+def main():
+    while True:
+        text = input("> ")
+        if text == "quit":
+            quit()
+        lexer = Lexer(text)
+        print(lexer.tokenize())
+        parser = Parser(lexer.tokenize())
+        parser.parse()
+
+
+if __name__ == "__main__":
+    main()
